42e7fffb6c2ff2e7d70fc10b7d16775c903729c4
Lucas Ramirez <lucas.ramirez@live.fr>
2018-11-04 18:51:22 +0100

    Fixed styling issue with contacts
    Contacts now have correct behavior when being selected/hovered

--- frontend/events.js
+++ frontend/events.js
@@ -1,7 +1,7 @@
-function switch_conv(contact) {
+function contact_switch() {
 
     // If the contact was already selected, do nothing
-    if (curr_contact !== null && contact.innerHTML === curr_contact.innerHTML) {
+    if (curr_contact !== null && this.innerHTML === curr_contact.innerHTML) {
         return
     }
 
@@ -14,18 +14,44 @@ function switch_conv(contact) {
     }
   
     // Select new contact and show new conversation
-    contact.style.backgroundColor = 'rgb(' + 66 + ',' + 70 + ',' + 77 + ')';
-    contact.style.color = 'rgb(' + 255 + ',' + 255 + ',' + 255 + ')';
-    var newConv = document.getElementById(contact.innerHTML)
+    this.style.backgroundColor = 'rgb(' + 66 + ',' + 70 + ',' + 77 + ')';
+    this.style.color = 'rgb(' + 255 + ',' + 255 + ',' + 255 + ')';
+    var newConv = document.getElementById(this.innerHTML)
     newConv.style.display = "block"
+    // TODO: change name in send message box
 
     // Update current contact
-    curr_contact = contact
+    curr_contact = this
 
+}
+
+function contact_mouse_enter() {
+    if (curr_contact === null || curr_contact.innerHTML != this.innerHTML) {
+        this.style.backgroundColor = 'rgb(' + 54 + ',' + 57 + ',' + 63 + ')';
+        this.style.color = 'rgb(' + 255 + ',' + 255 + ',' + 255 + ')';
+    }
+}
+
+function contact_mouse_leave() {
+    if (curr_contact === null || curr_contact.innerHTML != this.innerHTML) {
+        this.style.backgroundColor = 'rgb(' + 47 + ',' + 49 + ',' + 54 + ')';
+        this.style.color = 'rgb(' + 105 + ',' + 106 + ',' + 110 + ')';
+    }
+}
+
+function contact_attach_listeners(contact) {
+    contact.addEventListener("click", contact_switch);
+    contact.addEventListener("mouseenter", contact_mouse_enter);
+    contact.addEventListener("mouseleave", contact_mouse_leave);
 }
 
 var curr_contact = null
 window.onload = function(){
-    var tmp = document.getElementById("default_conv")
-    switch_conv(tmp)
+    
+    // Attach event listeners to all existing contacts
+    var contacts = document.getElementsByClassName("private_contact_wrap");
+    for (var i = 0 ; i < contacts.length ; i++) {
+        contact_attach_listeners(contacts[i]);
+    }
+    
 };
