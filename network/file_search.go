8a08f6c217918d7d5dd479ef5c7eb972ed9fcca7
Lucas Ramirez <lucas.ramirez@live.fr>
2018-11-19 03:32:06 +0100

    Structre of file_search.go

---
+++ network/file_search.go
@@ -0,0 +1,30 @@
+package network
+
+/* ================ SEARCH REQUEST ================ */
+
+// OnInitiateFileSearch initiates a file search on the network.
+func OnInitiateFileSearch() {
+
+}
+
+// OnSendSearchRequest sends a SearchRequest on the network.
+func OnSendSearchRequest() {
+
+}
+
+// OnReceiveSearchRequest handles an incoming SearchRequest.
+func OnReceiveSearchRequest() {
+
+}
+
+/* ================ SEARCH REPLY ================ */
+
+// OnSendSearchReply sends a SearchReply on the network.
+func OnSendSearchReply() {
+
+}
+
+// OnReceiveSearchReply handles an incoming SearchReply.
+func OnReceiveSearchReply() {
+
+}
