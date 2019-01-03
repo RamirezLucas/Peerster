85a7add4a04cb822d7619f12d0239b3d7321b404
Gregoire Clement <gregoire.clement@epfl.ch>
2019-01-03 21:50:15 +0100

    adding an updates package containing a specific implementation of channels

---
+++ updates/string_chan.go
@@ -0,0 +1,37 @@
+package updates
+
+/** This is a template of an easy to use channel with NON-blocking Push and blocking Get
+ *  It can be activated or not, an unactivated channel is doing nothing
+ *	and Get(s) are still blocking (and never resolves)
+ *
+ *  Only T, defaultValue, interface name and function name NewXxxChan needs to be change for easy reuse
+ */
+
+type T = string
+
+const defaultValue = ""
+
+type StringChan interface {
+	Get() T
+	Push(file T)
+}
+
+type matchChan struct {
+	Chan
+}
+
+func NewStringChan(activated bool) StringChan {
+	return &matchChan{Chan: NewChan(activated)}
+}
+
+func (ch *matchChan) Push(match T) {
+	ch.Chan.Push(match)
+}
+
+func (ch *matchChan) Get() T {
+	match, ok := ch.Chan.Get().(T)
+	if !ok {
+		return defaultValue
+	}
+	return match
+}
